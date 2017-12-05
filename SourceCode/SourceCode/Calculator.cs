f3f2cf0df607fa462d03c0cade5b637f438e4203
Nguyen Tran <nguyen.tran.forte1609@mail.com>
2017-12-05 20:48:20 +0700

    Complete Calculating CA function | Refactoring code

--- SourceCode/SourceCode/Calculator.cs
+++ SourceCode/SourceCode/Calculator.cs
@@ -8,213 +8,147 @@ using System.Windows.Forms;
 namespace SourceCode
 {
     class Calculator
-    {
-        protected List<List<int>> UM;
-        protected List<List<int>> AF;
-        protected List<List<int>> AA;
-        protected List<List<int>> CA;        
+    {    
         public Calculator()
         {
-            UM = new List<List<int>>();
-            AF = new List<List<int>>();
-            AA = new List<List<int>>();
-            CA = new List<List<int>>();
+
         }
-
-        internal void GetData(DataGridView data, string name)
-        {
-            List<List<int>> matrix = new List<List<int>>();
-            for (int row = 0; row < data.RowCount; row++)
-            {
-                List<int> row_matrix = new List<int>();
-                for (int col = 0; col < data.ColumnCount; col++)
-                    row_matrix.Add(int.Parse(data.Rows[row].Cells[col].Value.ToString()));
-                matrix.Add(row_matrix);
-            }
-            if (name == "UM")
-                this.UM = matrix;
-            if (name == "AF")
-                this.AF = matrix;
-        }
-
-        internal void Calculate()
-        {
-            if (CheckDataValidity())
-            {
-                Create_AA();
-                CalculateAA();
-                CalculateCA();
-            }
-        }
-
-
 
         #region Calculation
-        private void CalculateAA()
+        #region Calculate AA
+        internal void CalculateAA(DataGridView dataUM, DataGridView dataAF, DataGridView dataAA)
         {
+            dataAA.RowCount = dataUM.ColumnCount;
+            dataAA.ColumnCount = dataUM.ColumnCount;
             int checkpoint = 0; //used to by pass duplicate cases. For example: calculating A[1][0] and A[0][1]
-            for (int i = 0; i < AA.Count; i++)
+            for (int i = 0; i < dataAA.RowCount; i++)
             {
-                for (int j = checkpoint; j < AA[0].Count; j++)
+                for (int j = checkpoint; j < dataAA.Rows[i].Cells.Count; j++)
                 {
-                    int val = FindUsage(i, j);
-                    AA[i][j] = val;
-                    AA[j][i] = val;
+                    int val = FindUsage(dataUM, dataAF, i, j);
+                    dataAA.Rows[i].Cells[j].Value = val;
+                    dataAA.Rows[j].Cells[i].Value = val;
                 }
                 checkpoint++;
             }
         }
 
-        private int FindUsage(int first_A, int second_A)
+        private int FindUsage(DataGridView dataUM, DataGridView dataAF, int first_A, int second_A)
         {
+            //___If two attributes have access by queries, get the sum of that query access frequencies.
             int res = 0;
-            for (int i = 0; i < UM.Count; i++)
+            for (int i = 0; i < dataUM.RowCount; i++)
             {
-                if (UM[i][first_A] == 1 && UM[i][second_A] == 1)
+                if (int.Parse(dataUM.Rows[i].Cells[first_A].Value.ToString()) == 1 
+                    && int.Parse(dataUM.Rows[i].Cells[second_A].Value.ToString()) == 1)
                 {
-                    res += GetSumQueriesAccess(i);
+                    res += GetSumQueriesAccess(dataAF,i);
                 }
             }
             return res;
         }
 
-        private int GetSumQueriesAccess(int row)
+        private int GetSumQueriesAccess(DataGridView dataAF, int idxRow)
         {
+            //___Calculate sum of access frequencies of a given query
             int res = 0;
-            for (int i = 0; i < AF[0].Count; i++)
-                res += AF[row][i];
-            return res;
-        }
-
-        private void CalculateCA()
-        {
-            List<int> A0 = new List<int>();
-            List<int> order = new List<int>();
-            int highestCont = 0;
-            for (int i = 0; i < AA.Count; i++)
-                A0.Add(0);
-
-            CA.Add(AA[0]);          //obviously
-            CA.Add(AA[1]);
-            int checkpointCol = 2;  //used as a counter to present how many column that we have placed in CA
-            for(int rowAA = 2; rowAA < AA.Count; rowAA++) //Since AA is symmetric matrix, row <=> col
-            {
-                for(int rowCA = 0; rowCA < checkpointCol; rowCA++)
-                {
-                    if (rowCA == 0)
-                    {
-                        int cont = CalContribution(A0, CA[rowCA], AA[rowAA], highestCont);
-                        highestCont = cont;
-                        order.Clear();
-                        order.Add(0);
-                        order.Add(rowCA);
-                        order.Add(rowAA);
-                    }
-                    else if (rowCA == checkpointCol-1)
-                    {
-                        int cont = CalContribution(CA[rowCA], AA[rowAA], A0, highestCont);
-                        if (cont > highestCont)
-                        {
-                            highestCont = cont;
-                            order.Clear();
-                            order.Add(rowCA);
-                            order.Add(rowAA);
-                            order.Add(0);
-                        }
-                    }
-                    else
-                    {
-                        int cont = CalContribution(CA[rowCA], AA[rowAA], CA[rowCA+1], highestCont);
-                        if (cont > highestCont)
-                        {
-                            highestCont = cont;
-                            order.Clear();
-                            order.Add(rowCA);
-                            order.Add(rowAA);
-                            order.Add(rowCA+1);
-                        }
-                    }
-                }
-                if (order[0] == 0)
-                    CA.Insert(0, AA[rowAA]);
-                else if (order[2] == 0)
-                    CA.Add(AA[rowAA]);
-                else
-                    CA.Insert(order[0]+1, AA[rowAA]);
-                highestCont = 0;
-                order.Clear();
-                checkpointCol++;
-            }
-        }
-
-        private int CalContribution(List<int> left, List<int> mid, List<int> right, int highestCont)
-        {
-            return 2 * bond(left, mid) + 2 * bond(mid, right) - 2 * bond(left, right);
-        }
-
-        private int bond(List<int> col1, List<int> col2)
-        {
-            int res = 0;
-            for (int i = 0; i < col1.Count; i++)
-                res += col1[i] * col2[i];
+            for (int i = 0; i < dataAF.ColumnCount; i++)
+                res += int.Parse(dataAF.Rows[idxRow].Cells[i].Value.ToString());
             return res;
         }
         #endregion
 
-
-
-        #region Utilities
-        private void Create_AA()
+        #region Calculate CA
+        internal List<int> CalculateCA(DataGridView dataAA)
         {
-            //Create sample AA, CA for later use
-            List<List<int>> matrix = new List<List<int>>();
-            for (int row = 0; row < UM.Count; row++)
+            DataGridViewRow ZeroRow = new DataGridViewRow();//This is A0 
+            for (int i = 0; i < dataAA.RowCount; i++)       //Add zero cells to A0
             {
-                List<int> row_matrix = new List<int>();
-                for (int col = 0; col < UM[row].Count; col++)
-                    row_matrix.Add(0);
-                matrix.Add(row_matrix);
+                ZeroRow.Cells.Add(new DataGridViewTextBoxCell { Value = 0 });
             }
-            AA = matrix;
-        }
-
-        internal DataGridView ExportAAMatrix()
-        {
-            DataGridView newMatrix = new DataGridView();
-            newMatrix.RowCount = AA.Count;
-            newMatrix.ColumnCount = AA[0].Count;
-            for (int row = 0; row < newMatrix.RowCount; row++)
+            List<int> orderList = new List<int>();  //List of row order <=> column order
+            //___Place column 1 and 2 of AA in CA
+            orderList.Add(0);
+            orderList.Add(1);
+            for (int rowAA = 2; rowAA < dataAA.ColumnCount; rowAA++) //Since AA is symmetric matrix, row <=> col. 
             {
-                List<int> row_matrix = new List<int>();
-                for (int col = 0; col < newMatrix.ColumnCount; col++)
-                    newMatrix.Rows[row].Cells[col].Value = AA[col][row];
+                int highestCont = 0;    //Highest contribution
+                int position = 0;       //Position of current row which have highest contribution
+                for (int i = 0; i < orderList.Count; i++)
+                {
+                    if (i == 0)
+                    {
+                        // Case (0,Ai,A1)
+                        highestCont = CalculateCont(ZeroRow, dataAA.Rows[rowAA], dataAA.Rows[orderList[i]]);
+                        position = i;
+                    }
+                    if (i != 0)
+                    {
+                        int currentCont = 0;
+                        if (i == orderList.Count - 1)
+                        {
+                            // Case (An,Ai,0)
+                            currentCont = CalculateCont(dataAA.Rows[orderList[i]], dataAA.Rows[rowAA], ZeroRow);
+                        }
+                        if (i != orderList.Count - 1)
+                        {
+                            // Normal Case
+                            currentCont = CalculateCont(dataAA.Rows[orderList[i]], dataAA.Rows[rowAA], dataAA.Rows[orderList[i + 1]]);
+                        }
+                        // if current contribution is higher, change value of highest contribution and position 
+                        if (highestCont < currentCont)
+                        {
+                            highestCont = currentCont;
+                            position = i;
+                        }
+                    }
+                }
+                orderList.Insert(position+1, rowAA);    //Add position of crrent highest contribution to list
             }
-            return newMatrix;
+            DataGridView CA = RearrangeAAUsingListOrder(orderList,dataAA);
+            return orderList;
         }
 
-        internal DataGridView ExportCAMatrix()
+        private int CalculateCont(DataGridViewRow left, DataGridViewRow mid, DataGridViewRow right)
         {
-            DataGridView newMatrix = new DataGridView();
-            newMatrix.RowCount = CA.Count;
-            newMatrix.ColumnCount = CA[0].Count;
-            for (int row = 0; row < newMatrix.RowCount; row++)
-            {
-                List<int> row_matrix = new List<int>();
-                for (int col = 0; col < newMatrix.ColumnCount; col++)
-                    newMatrix.Rows[row].Cells[col].Value = CA[col][row];
-            }
-            return newMatrix;
+            return 2 * CalculateBond(left, mid) + 2 * CalculateBond(mid, right) - 2 * CalculateBond(left, right);
         }
 
-        private bool CheckDataValidity()
+        private int CalculateBond(DataGridViewRow left, DataGridViewRow mid)
         {
-            if (this.UM.Count < 0
-                || this.AF.Count < 0
-                || this.AA.Count < 0
-                || this.CA.Count < 0)
-                return false;
-            return true;
+            int result = 0;
+            for (int i = 0; i < left.Cells.Count; i++)
+                result += int.Parse(left.Cells[i].Value.ToString()) * int.Parse(mid.Cells[i].Value.ToString());
+            return result;
         }
+
+        private DataGridView RearrangeAAUsingListOrder(List<int> orderList, DataGridView dataAA)
+        {
+            //Rearrange columns
+            for(int i = 0; i < dataAA.RowCount; i++)
+                for(int j = 0; j < dataAA.ColumnCount; j++)
+                {
+                    if(dataAA.Rows[i].Cells[j].ColumnIndex < orderList[j])
+                    {
+                        object temp = dataAA.Rows[i].Cells[j].Value;
+                        dataAA.Rows[i].Cells[j].Value = dataAA.Rows[i].Cells[orderList[j]].Value;
+                        dataAA.Rows[i].Cells[orderList[j]].Value = temp;
+                    }
+                }
+            //Rearrange rows
+            for (int i = 0; i < dataAA.RowCount; i++)
+                for (int j = 0; j < dataAA.ColumnCount; j++)
+                {
+                    if (dataAA.Rows[i].Cells[j].RowIndex < orderList[i])
+                    {
+                        object temp = dataAA.Rows[i].Cells[j].Value;
+                        dataAA.Rows[i].Cells[j].Value = dataAA.Rows[orderList[i]].Cells[j].Value;
+                        dataAA.Rows[orderList[i]].Cells[j].Value = temp;
+                    }
+                }
+            return dataAA;
+        }
+        #endregion
         #endregion
     }
 }
--- SourceCode/SourceCode/mainFrame.cs
+++ SourceCode/SourceCode/mainFrame.cs
@@ -32,18 +32,23 @@ namespace SourceCode
         #region Main Functions
         private void buttonCreateMatrix_Click(object sender, EventArgs e)
         {
+            //___Create matrices: UM, AF, AA, CA using user's input values
             if (checkInput())
             {
                 DrawMatrix(dataUM, int.Parse(textQueries.Text), int.Parse(textAttributes.Text));
                 NameRowAndColumn(dataUM, 'q', 'A');
                 DrawMatrix(dataAF, int.Parse(textQueries.Text), int.Parse(textSites.Text));
                 NameRowAndColumn(dataAF, 'q', 'S');
-
+                DrawMatrix(dataAA, int.Parse(textAttributes.Text), int.Parse(textAttributes.Text));
+                NameRowAndColumn(dataAA, 'A', 'A');
+                DrawMatrix(dataCA, int.Parse(textAttributes.Text), int.Parse(textAttributes.Text));
+                NameRowAndColumn(dataCA, 'A', 'A');
             }
         }
 
         private void btnClear_Click(object sender, EventArgs e)
         {
+            //___Clear all datagridviews
             dataUM.Rows.Clear();
             dataUM.Columns.Clear();
             dataAF.Rows.Clear();
@@ -57,18 +62,26 @@ namespace SourceCode
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
-            myCal.GetData(dataUM, "UM");
-            myCal.GetData(dataAF, "AF");
-            myCal.Calculate();
-            DataGridView AA = myCal.ExportAAMatrix();
-            DataGridView CA = myCal.ExportCAMatrix();
-            DisplayMatrix(dataAA, AA);
-            DisplayMatrix(dataCA, CA);
-
+            //___Calculate AA matrix
+            myCal.CalculateAA(dataUM, dataAF, dataAA);
+            //___Copy AA to CA
+            for(int i = 0; i < dataAA.RowCount; i++)
+            {
+                for (int j = 0; j < dataAA.Rows[i].Cells.Count; j++)
+                {
+                    dataCA.Rows[i].Cells[j].Value = dataAA.Rows[i].Cells[j].Value;
+                }
+            }
+            //___Calculate CA matrix and return list of column order
+            List<int> orderList = myCal.CalculateCA(dataCA);
+            NameRowAndColumn(dataCA,'A','A', orderList);    // name CA columns
+            //___Update views
+            dataAA.Update();
+            dataAA.Refresh();
+            dataCA.Update();
+            dataCA.Refresh();
         }
         #endregion
-
-
 
         #region Utilities
         private void DrawMatrix(DataGridView dataView, int row, int col)
@@ -77,8 +90,9 @@ namespace SourceCode
             dataView.ColumnCount = col;
         }
 
-        private bool checkInput()
+        private bool checkInput() 
         {
+            //___Check user's input values. It's will be invalid if it's not able to convert to integer
             if (textAttributes.Text != "" && textQueries.Text != "" && textSites.Text != "")
             {
                 int temp1, temp2, temp3;
@@ -90,21 +104,22 @@ namespace SourceCode
             return false;
         }
 
-        private void NameRowAndColumn(DataGridView data, char rol, char col)
+        private void NameRowAndColumn(DataGridView data, char rol, char col, List<int> orderList = null)
         {
-            for (int i = 0; i < data.ColumnCount; i++)
-                data.Columns[i].HeaderText = col + (i + 1).ToString();
-            for (int i = 0; i < data.RowCount; i++)
-                data.Rows[i].HeaderCell.Value = rol + (i + 1).ToString();
-        }
-
-        private void DisplayMatrix(DataGridView data, DataGridView source)
-        {
-            DrawMatrix(data, source.RowCount, source.ColumnCount);
-            for (int i = 0; i < data.RowCount; i++)
-                for (int j = 0; j < data.Rows[i].Cells.Count; j++)
-                    data.Rows[i].Cells[j].Value = source.Rows[i].Cells[j].Value;
-            NameRowAndColumn(data, 'A', 'A');
+            if (orderList == null)  // In case of UM, AF, AA matrix
+            {
+                for (int i = 0; i < data.ColumnCount; i++)  // Name columns
+                    data.Columns[i].HeaderText = col + (i + 1).ToString();
+                for (int i = 0; i < data.RowCount; i++)     //Name rows
+                    data.Rows[i].HeaderCell.Value = rol + (i + 1).ToString();
+            }
+            if (orderList != null)  // In case of CA matrix
+            {
+                for (int i = 0; i < dataCA.ColumnCount; i++)    // Name column
+                    dataCA.Columns[i].HeaderText = col + (orderList[i] + 1).ToString();
+                for (int i = 0; i < dataCA.RowCount; i++)       //Name rows
+                    dataCA.Rows[i].HeaderCell.Value = col + (orderList[i] + 1).ToString();
+            }
         }
     }
     #endregion
